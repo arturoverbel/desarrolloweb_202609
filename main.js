de2b668b582f44e0c0d8cbd32845902dad2d60b2
Arturo Verbel <arturo.verbel@veritasdataresearch.com>
2026-09-17 14:37:57 -0500

    Primea version Backend server

---
+++ main.js
@@ -0,0 +1,45 @@
+
+import express from "express";
+import cors from "cors";
+
+
+const app = express();
+app.use(cors())
+
+const PORT = 3000;
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send("From server 🚀")
+})
+
+app.get('/api/grettings', (req, res) => {
+    res.json({
+        message: "Hii, from Node Server 👋"
+    })
+})
+
+app.get('/api/servers', (req, res) => {
+    res.json([
+        { id: 1, name: "SEO", price: 150 },
+        { id: 2, name: "Web Hosting", price: 100 },
+        { id: 3, name: "VPS", price: 200 },
+        { id: 4, name: "Dedicated Server", price: 300 }
+    ])
+})
+
+
+app.post('/api/contact', (req, res) => {
+    const { name, email, message } = req.body
+
+    console.log("New messsage: ", name, email, message)
+
+    res.json({
+        status: 'ok'
+    })
+})
+
+app.listen(PORT, () => {
+    console.log("Server Listening ....")
+})
